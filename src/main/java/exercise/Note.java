e39ee02f6cf0017a79c97b02107c162e21c73ecd
zahra <zahra.hazara@metropolia.fi>
2024-05-06 22:21:48 +0300

    Initial commit

---
+++ src/main/java/exercise/Note.java
@@ -0,0 +1,17 @@
+package exercise;
+public class Note {
+    private String title;
+    private String content;
+
+    public Note(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    // Getters and setters
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public String getContent() { return content; }
+    public void setContent(String content) { this.content = content; }
+}
+
