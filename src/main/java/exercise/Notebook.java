f33362c14e1dec256d14aae2ea9cf49d438f9bdf
zahra-hazara <157361068+zahra-hazara@users.noreply.github.com>
2025-02-25 12:17:04 +0200

    Update Notebook.java

--- src/main/java/exercise/Notebook.java
+++ src/main/java/exercise/Notebook.java
@@ -1,22 +1,2 @@
-package exercise;
 
-import exercise.Note;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class Notebook {
-    private List<Note> notes;
-
-    public Notebook() {
-        notes = new ArrayList<>();
-    }
-
-    public void addNote(Note note) {
-        notes.add(note);
-    }
-
-    public List<Note> getNotes() {
-        return notes;
-    }
-}
