d0025d93f08454de8435aa3876fdf07bd31c4016
zahra-hazara <157361068+zahra-hazara@users.noreply.github.com>
2025-02-25 12:15:54 +0200

    Update Note.java

--- src/main/java/exercise/Note.java
+++ src/main/java/exercise/Note.java
@@ -1,17 +1,1 @@
-package exercise;
-public class Note {
-    private String title;
-    private String content;
-
-    public Note(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    // Getters and setters
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-    public String getContent() { return content; }
-    public void setContent(String content) { this.content = content; }
-}
 
